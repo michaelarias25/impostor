ce917d6ece1eeb4f3e14815b7b0ce48f21de768a
Michael Arias <michaelarias20115@gmail.com>
2026-02-20 10:53:00 -0500

    Initial commit

---
+++ src/main/java/edu/co/ustavillavicencio/impostor/ImpostorApplication.java
@@ -0,0 +1,13 @@
+package edu.co.ustavillavicencio.impostor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpostorApplication.class, args);
+    }
+
+}
